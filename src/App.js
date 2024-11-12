3b507d8417bd5afca51291804f6227564392e218
Ginever <86098178+Ginever@users.noreply.github.com>
2024-11-13 00:13:33 +1300

    made it pretty and added base functionality

--- src/App.js
+++ src/App.js
@@ -1,42 +1,156 @@
-import React, { useState, useEffect } from 'react';
-import { TextField, Button } from '@mui/material';
-import Todo from './components/Todo';
-import { db } from './firebase.js';
-import { collection, query, orderBy, onSnapshot, addDoc, serverTimestamp } from 'firebase/firestore';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  TextField,
+  Button,
+  Fab,
+  Modal,
+  Box,
+  List,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import Todo from "./components/Todo";
+import { db } from "./firebase.js";
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  addDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+import "./App.css";
 
-const q = query(collection(db, 'todos'), orderBy('timestamp', 'desc'));
+const q = query(collection(db, "todos"), orderBy("timestamp", "desc"));
+
 function App() {
-    const [todos, setTodos] = useState([]);
-    const [input, setInput] = useState('');
-    useEffect(() => {
-        onSnapshot(q, (snapshot) => {
-            setTodos(snapshot.docs.map(doc => ({
-                id: doc.id,
-                item: doc.data()
-            })))
-        })
-    }, [input]);
-    const addTodo = (e) => {
-        e.preventDefault();
-        addDoc(collection(db, 'todos'), {
-            todo: input,
-            timestamp: serverTimestamp()
-        })
-        setInput('')
-    };
-    return (
-        <div className="App">
-            <h2> TODO List App</h2>
-            <form>
-                <TextField id="outlined-basic" label="Make Todo" variant="outlined" style={{ margin: "0px 5px" }} size="small" value={input}
-                    onChange={e => setInput(e.target.value)} />
-                <Button variant="contained" color="primary" onClick={addTodo}  >Add Todo</Button>
-            </form>
-            <ul>
-                {todos.map(item => <Todo key={item.id} arr={item} />)}
-            </ul>
-        </div>
-    );
+  const [todos, setTodos] = useState([]);
+  const [inputs, setInputs] = useState({
+    task: null,
+    priority: null,
+    quantity: null,
+    isBagged: null,
+  });
+  const [openModal, setOpenModal] = useState(false);
+
+  useEffect(() => {
+    onSnapshot(q, (snapshot) => {
+      setTodos(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          item: doc.data(),
+        }))
+      );
+    });
+  }, [inputs]);
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    var value = event.target.value;
+
+    if (name == "isBagged") {
+      value = !inputs.isBagged;
+    }
+
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    addDoc(collection(db, "todos"), {
+      task: inputs.task,
+      priority: inputs.priority,
+      isBagged: inputs.isBagged,
+      quantity: inputs.quantity,
+      timestamp: serverTimestamp(),
+    });
+    closeModal();
+  };
+
+  const closeModal = () => {
+    setOpenModal(false);
+    setInputs({ task: null, priority: null, quantity: null, isBagged: null });
+  };
+  return (
+    <>
+      <div className="App">
+        <h2 className="text-2xl">Bagging list</h2>
+        <List className="w-full">
+          {todos.map((item) => (
+            <Todo key={item.id} arr={item} />
+          ))}
+        </List>
+        <Fab
+          className="floating-action-button"
+          color="primary"
+          aria-label="add"
+          onClick={() => setOpenModal(true)}
+        >
+          <AddIcon />
+        </Fab>
+      </div>
+      <Modal open={openModal} onClose={() => setOpenModal(false)}>
+        <Box className="modal-box ">
+          <form>
+            <TextField
+              id="outlined-basic"
+              label="Description"
+              name="task"
+              variant="outlined"
+              style={{ margin: "0px 5px" }}
+              size="small"
+              value={inputs.task}
+              onChange={(e) => handleChange(e)}
+            />
+
+            <TextField
+              type="number"
+              id="outlined-basic"
+              label="Quantity"
+              variant="outlined"
+              style={{ margin: "0px 5px" }}
+              size="small"
+              value={inputs.quantity}
+              name="quantity"
+              onChange={(e) => handleChange(e)}
+            />
+
+            <TextField
+              type="number"
+              id="outlined-basic"
+              label="Priority"
+              variant="outlined"
+              style={{ margin: "0px 5px" }}
+              size="small"
+              value={inputs.priority}
+              name="priority"
+              onChange={(e) => handleChange(e)}
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  margin="auto"
+                  name="isBagged"
+                  checked={inputs.isBagged}
+                  onChange={(e) => handleChange(e)}
+                />
+              }
+              label="isBagged"
+            />
+
+            <Button
+              className="my-10"
+              variant="contained"
+              color="primary"
+              onClick={addTodo}
+            >
+              Add Todo
+            </Button>
+          </form>
+        </Box>
+      </Modal>
+    </>
+  );
 }
 export default App;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,22 +1,47 @@
-import { List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import {
+  ListItemText,
+  IconButton,
+  ListItemButton,
+  Box,
+  List,
+} from "@mui/material";
+import ListItem from "@mui/material/ListItem";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { db } from "../firebase.js";
-import { doc, deleteDoc } from "firebase/firestore";
+import { doc, deleteDoc, setDoc } from "firebase/firestore";
+
 const Todo = ({ arr }) => {
   return (
-    <List className="todo__list">
-      <ListItem>
-        <ListItemAvatar />
-        <ListItemText primary={arr.item.todo} secondary={arr.item.todo} />
-      </ListItem>
-      <DeleteIcon
-        fontSize="large"
-        style={{ opacity: 0.7 }}
-        onClick={() => {
-          deleteDoc(doc(db, "todos", arr.id));
-        }}
+    <ListItem
+      className="outline m-3 rounded table border-20"
+      style={{
+        width: "auto",
+        color: arr.item.isBagged ?? false ? "black" : "red",
+      }}
+      button
+      onClick={() => {
+        setDoc(doc(db, "todos", arr.id), {
+          ...arr.item,
+          isBagged: !arr.item.isBagged,
+        });
+        console.log("clicked" + arr.item.isBagged);
+      }}
+    >
+      <ListItemText
+        primary={arr.item.task ?? "Someone forgot the name"}
+        secondary={"quantity: " + (arr.item.quantity ?? "?")}
       />
-    </List>
+      <ListItemButton class="float-right mr-3" edge="end">
+        <IconButton edge="end" aria-label="delete">
+          <DeleteIcon
+            edge="end"
+            onClick={() => {
+              deleteDoc(doc(db, "todos", arr.id));
+            }}
+          />
+        </IconButton>
+      </ListItemButton>
+    </ListItem>
   );
 };
 export default Todo;
